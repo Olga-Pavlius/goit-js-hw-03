f552c2ff84584adb25f4b77973747b6faf4ba1fe
Olga Pavlius <olya.melnyk.9412@gmail.com>
2024-11-16 00:40:54 +0200

    home work 3

---
+++ js/task-1.js
@@ -0,0 +1,11 @@
+function slugify(title) {
+   
+    const slugiArr = title.split(" ");
+    const slug = slugiArr.join("-");
+return slug;
+} 
+
+console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
